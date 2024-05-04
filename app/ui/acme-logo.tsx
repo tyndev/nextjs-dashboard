4ab8c92229822cc11f0484cc1dcf9758146ca1a7
tyndev <17089219+tyndev@users.noreply.github.com>
2024-05-04 10:55:26 -0700

    modify logo

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,43 @@
-import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { GlobeAltIcon } from '@heroicons/react/24/outline';
+// import { lusitana } from '@/app/ui/fonts';
+
+// export default function AcmeLogo() {
+//   return (
+//     <div
+//       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+//     >
+//       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+//       <p className="text-[44px]">Acme</p>
+//     </div>
+//   );
+// }
+
 import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
+    <div className={`${lusitana.className} flex flex-row items-center leading-none text-white `}>
+      <div className="flex flex-col items-center">
+          <span className="text-[62px] transform rotate-[185deg]">🗸🗸</span>
+          <p className="text-[32px] pt-2">VectorSearch</p>
+      </div>
     </div>
   );
 }
+
+
+// import { lusitana } from '@/app/ui/fonts';
+
+// export default function AcmeLogo() {
+//   return (
+//     <div className={`${lusitana.className} flex flex-row items-center leading-none text-white`}>
+//       <div className="transform rotate-[15deg]">
+//         <span className="text-[32px]">⤛</span>
+//       </div>
+//       <p className="text-[44px] mx-2">Acme</p>
+//       <div className="transform -rotate-[15deg]">
+//         <span className="text-[32px]">⤜</span>
+//       </div>
+//     </div>
+//   );
+// }
